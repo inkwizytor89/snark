726d4bd251de799b739120d9a45ac6e56e1723e3
Kamil Kostyszyn <inkwizytor89@gmail.com>
2022-09-14 21:14:27 +0200

    refactor

--- src/main/java/org/enoch/snark/gi/command/impl/GalaxyAnalyzeCommand.java
+++ src/main/java/org/enoch/snark/gi/command/impl/GalaxyAnalyzeCommand.java
@@ -6,33 +6,20 @@ import org.enoch.snark.db.entity.GalaxyEntity;
 import org.enoch.snark.gi.command.CommandType;
 import org.enoch.snark.gi.command.GICommand;
 import org.enoch.snark.gi.macro.GIUrlBuilder;
-import org.enoch.snark.instance.Instance;
 import org.enoch.snark.instance.Utils;
-import org.enoch.snark.model.SystemView;
-
-import java.util.Optional;
-
-import static org.enoch.snark.model.Universe.GALAXY_MAX;
 
 public class GalaxyAnalyzeCommand extends GICommand {
 
     private final GalaxyEntity galaxyEntity;
     private GalaxyDAO galaxyDAO;
     private final GIUrlBuilder giUrlBuilder;
-//    private SystemView systemView;
 
     public GalaxyAnalyzeCommand(GalaxyEntity galaxyEntity) {
         super(CommandType.INTERFACE_REQUIERED);
         this.galaxyEntity = galaxyEntity;
         galaxyDAO = GalaxyDAO.getInstance();
         giUrlBuilder = new GIUrlBuilder();
-//        this.systemView = systemView;
-//        normalize(this.systemView);
     }
-
-//    private void normalize(SystemView systemView) {
-//        systemView.galaxy = Math.floorMod(systemView.galaxy, Integer.parseInt(instance.universe.getConfig(GALAXY_MAX)));
-//    }
 
     @Override
     public boolean execute() {
